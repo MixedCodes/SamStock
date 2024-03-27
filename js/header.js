ae1d84e42e02d28d36af05b0f2984d4b6bf2cc85
Patipan Tangmongkonpaisan <patipan1528@gmail.com>
2024-03-27 22:47:51 +0700

    fix: update main.css and header.js

--- js/header.js
+++ js/header.js
@@ -4,14 +4,13 @@
 const headerTemplate = document.createElement("template");
 
 headerTemplate.innerHTML = `
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/main.css"/>
 	<style>
 		nav {
-			padding: 0 16px;
+			gap: 0 1rem;
 		}
 
-		nav ul, .container {
+		nav > ul, .container, header > nav, header form {
 			display: flex;
 			flex-flow: row nowrap;
 			align-items: center;
@@ -23,50 +22,73 @@ headerTemplate.innerHTML = `
 
 		nav a {
 			color: #000000;
-			padding: 0 16px;
+			padding: 0 1rem;
 			text-decoration: none;
+
+			&:hover {
+				-webkit-text-stroke-width: 0.1ex;
+			}
 		}
 
-		#upper-nav {
+		#top-nav {
 			background-color: #FFE270;
+			padding: 0.25px 0;
 		}
 
-		#upper-nav ul {
-			height: 30px;
+		#top-nav ul {
 			justify-content: flex-end;
 		}
 
 		header nav {
-			display: flex;
-			flex-flow: row wrap;
+			position: relative;
+			z-index: 1;
 			justify-content: space-between;
-			align-items: center;
 			background-color: #FFFFFF;
-			box-shadow:0px 5px 5px 0 rgba(0,0,0,.16);
+			padding: 13px 0;
+			box-shadow:0 5px 5px 0 rgba(0,0,0,.16);
 		}
 
 		header nav ul {
 			align-items: center;
 		}
 
-		#logo {
-			padding: 13px 0;
+		header form {
+			justify-content: space-evenly;
+			width: 100%;
+			background-color: #F4F4F4;
+			border: none;
+		    border-radius: 1rem;
+		    padding: 0 1rem;
+		}
+
+		nav button {
+			background: transparent;
+			border: 0;
+			cursor: pointer;
 		}
 
 		nav input[type="search"] {
 			width: 100%;
-			height: 30px;
-			background-color: #F4F4F4;
+			height: 2rem;
+			background: transparent;
 			border: none;
-		    border-radius: 15px;
-		    padding: 0 10px;
+			border-radius: 1rem;
+
+			&:focus {
+				outline: none;
+			}
+		}
+
+		[type="search"]::-webkit-search-decoration,
+		[type="search"]::-webkit-search-cancel-button {
+		  appearance: none;
 		}
 	</style>
 
-	<nav id="upper-nav">
+	<nav id="top-nav">
 		<ul>
-			<li><a href="admin.html">Admin Management</a></li>
-			<li><a href="team.html">Team Page</a></li>
+			<li><a href="admin.html"><small>Admin Management</small></a></li>
+			<li><a href="team.html"><small>Team Page</small></a></li>
 		</ul>
 	</nav>
 	<header>
@@ -79,9 +101,12 @@ headerTemplate.innerHTML = `
 				<li><a href="sandals.html">Sandals</a></li>
 			</ul>
 			<div class="container">
-				<input type="search" placeholder="Search..."/>
-				<a href="login.html"><img src="assets/login-icon.png" alt="Login Icon" class="icon" width="45" height="45"/></a>
-			</div>
+				<form>
+					<input type="search" placeholder="Search..."/>
+					<button type="submit"><img src="assets/search-icon.svg" alt="Search Icon" class="icon" width="16" height="16"/></button>
+				</form>
+				<a href="login.html"><img src="assets/login-icon-test.png" alt="Login Icon" class="icon" width="32" height="32"/></a>
+			</div> 
 		</nav>
 	</header>`
 
